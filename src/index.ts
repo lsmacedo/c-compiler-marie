c7595e3022adf77cccb2f6b2a82e4bf4cd22f0e2
Lucas Macedo <lucas.silveira.2010@hotmail.com>
2023-07-27 13:42:43 -0300

    add 'compile' npm script

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import * as fs from "fs";
 import { parseCode } from "./parse";
 import { compileForMarieAssemblyLanguage } from "./compile";
 
-const code = fs.readFileSync("./examples/1-fibonacci.c", "utf-8");
+const filePath = process.argv[2];
+
+const code = fs.readFileSync(filePath, "utf-8");
 
 const parsedCode = parseCode(code);
 const assemblyCode = compileForMarieAssemblyLanguage(parsedCode);
