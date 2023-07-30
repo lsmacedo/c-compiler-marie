af281cb8f845f422927f78490094055ff53c00f6
Lucas Macedo <lucas.silveira.2010@hotmail.com>
2023-07-30 18:42:21 -0300

    Add CLI options with yargs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import * as fs from "fs";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
 import { parseCode } from "./parse";
 import { compileForMarieAssemblyLanguage } from "./compile";
 
-const filePath = process.argv[2];
+const args = yargs(hideBin(process.argv))
+  .option("verbose", {
+    alias: "v",
+    type: "boolean",
+    description: "Run with verbose logging",
+  })
+  .option("file", {
+    alias: "f",
+    type: "string",
+    demandOption: true,
+  })
+  .option("output", {
+    alias: "o",
+    type: "string",
+    default: "a.mas",
+  })
+  .parseSync();
 
-const code = fs.readFileSync(filePath, "utf-8");
+// Read file
+const code = fs.readFileSync(args.file, "utf-8");
 
+// Parse code
 const parsedCode = parseCode(code);
+if (args.verbose) {
+  console.log(JSON.stringify(parsedCode));
+}
+
+// Generate assembly code
 const assemblyCode = compileForMarieAssemblyLanguage(parsedCode);
-console.log(assemblyCode);
+fs.writeFileSync(args.output, assemblyCode);
